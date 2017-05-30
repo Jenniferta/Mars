de60ba8d8611e325b0e8ef13cd835184fa54e54a
Jennifer Ta <jenniferta90@gmail.com>
2017-05-30 09:30:28 -0700

    creating routers in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { RegisterComponent } from './page/register/register.component';
+import { EncountersComponent } from './page/encounters/encounters.component';
+import { ReportComponent } from './page/report/report.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'encounters', component: EncountersComponent },
+  { path: 'report', component: ReportComponent},
+  ];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    EncountersComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
